8e229b501b7cec6b7955440c483b05ae5b9ee699
muraliwebronic <muralikrishna.webronic@gmail.com>
2025-12-23 17:17:02 +0530

    price layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,4 @@
-import type { Metadata } from "next";
+import type { Metadata, Viewport } from "next";
 import "./globals.css";
 import NavBar from "@/components/NavBar";
 import Footer from "@/components/Footer";
@@ -9,6 +9,11 @@ export const metadata: Metadata = {
   title: "Virtual Tour 360",
   description: "Immersive Virtual Tours",
 };
+
+export const viewport: Viewport ={
+  themeColor:"#e7f6f26e"
+}
+
 
 export default function RootLayout({
   children,
